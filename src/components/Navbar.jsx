90636c145c0f6c97c01ac7bc33f828ce62a0a30b
Dan Garro <dobetterwithdan@gmail.com>
2023-04-08 15:32:33 -0400

    adjusted look of nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import React, {useState} from 'react'
 const Navbar = () => {
     const [name, setName] = useState("there");
   return (
-    <div className="flex flex-row bg-rose-50 items-center text-xl justify-between w-full h-24 px-5">
+    <div className="flex flex-row bg-blue-50 items-center text-xl justify-between w-full h-24 m-4 px-5 shadow-2xl shadow-indigo-500/50">
         Logo 
         <h2>Welcome, {name}</h2>
         <input onChange={(e) => setName(e.target.value)} />
-        <div className="flex gap-3">
+        <div className="flex gap-3 mr-2">
             <a>Home</a>
             <a>About</a>
             <a>Portfolio</a>
